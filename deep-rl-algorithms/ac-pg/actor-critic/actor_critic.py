33ba6ac03f14efd1fd67aa2e3cde1ff50040bb38
sheeerio <sheerio105@gmail.com>
2024-07-12 22:02:24 +0530

    actor-critic

---
+++ deep-rl-algorithms/ac-pg/actor-critic/actor_critic.py
@@ -0,0 +1,60 @@
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.distributions.categorical as Categorical
+
+class ActorCriticNetwork(nn.Module):
+    def __init__(self, input_dims, n_actions, lr=5e-6, fc1_dims=256, fc2_dims=256):
+        super(ActorCriticNetwork, self).__init__()
+        self.fc1 = nn.Linear(*input_dims, fc1_dims)
+        self.fc2 = nn.Linear(fc1_dims, fc2_dims)
+        self.actions = nn.Linear(fc2_dims, n_actions)
+        self.v = nn.Linear(fc2_dims, 1)
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'mps')
+        self.to(self.device)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        actions = self.actions(x)
+        v = self.v(x)
+
+        return actions, v
+
+class Agent():
+    def __init__(self, input_dims, n_actions, gamma=0.99, lr=5e-6):
+        self.lr = lr
+        self.gamma = gamma
+        self.actorcritic = ActorCriticNetwork(input_dims, n_actions)
+        self.log_prob = None
+
+    def choose_action(self, obs):
+        obs = T.tensor([obs], dtype=T.float).to(self.actorcritic.device)
+        actions, _ = self.actorcritic(obs)
+        probs = F.softmax(actions, dim=1)
+        action_probs = Categorical.Categorical(probs)
+        action = action_probs.sample()
+        self.log_prob = action_probs.log_prob(action)
+
+        return action.item()
+
+    def learn(self, state, reward, done, state_):
+        self.actorcritic.optimizer.zero_grad()
+
+        state = T.tensor([state], dtype=T.float).to(self.actorcritic.device)
+        reward = T.tensor(reward, dtype=T.float).to(self.actorcritic.device)
+        state_ = T.tensor([state_], dtype=T.float).to(self.actorcritic.device)
+
+        _, V_s = self.actorcritic(state)
+        _, V_s_ = self.actorcritic(state_)
+
+        delta = reward + self.gamma*V_s_*(1-int(done)) - V_s
+
+        actor_loss = -self.log_prob*delta
+        critic_loss = delta**2
+
+        (actor_loss+critic_loss).backward()
+        self.actorcritic.optimizer.step()
---
+++ deep-rl-algorithms/ac-pg/actor-critic/main.py
@@ -0,0 +1,39 @@
+from actor_critic import Agent
+import matplotlib.pyplot as plt
+import gymnasium as gym
+import numpy as np
+
+def plot_learning_curve(x, scores, filename):
+    running_avg = np.zeros(len(scores))
+    for i in range(len(scores)):
+        running_avg[i] = scores[max(0, i-100):(i+1)]
+    plt.plot(x, scores)
+    plt.title('Running average of prior 100 games')
+    plt.savefig(filename)
+
+if __name__ == '__main__':
+    env = gym.make('LunarLander-v2')
+    agent = Agent(input_dims=[8], n_actions=4)
+    n_games = 2000
+    fname = f'ActorCritic_lunar_lander_lr{agent.lr}_{n_games}games'
+    figfile = 'plots/' + fname + '.png'
+    scores = []
+
+    for i in range(n_games):
+        obs, _ = env.reset()
+        score = 0
+        done = False
+        while not done:
+            action = agent.choose_action(obs)
+            obs_, reward, done, *_ = env.step(action)
+            score += reward
+            agent.learn(obs, reward, done, obs_)
+            obs = obs_
+        scores.append(score)
+
+        avg_score = np.mean(scores[-100:])
+        print('episode ', i, 'score %.2f' % score,
+                'average score %.2f' % avg_score)
+
+    x = [i+1 for i in range(len(scores))]
+    plot_learning_curve(scores, x, figure_file)
